6becdfa08c3e2e9f8db3e2f0083b41abcecae5bb
Thomas McInnis <hey@thomasmcinnis.com>
2024-09-22 21:22:06 +1000

    Amend last

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("core.settings")
+require("core.keymaps")
+
+vim.cmd.colorscheme("catppuccin")
+
+-- vim: ts=2 sts=2 sw=2 et
