059ed74c68bd0eef74eeede40962c54bbc10be36
Thomas McInnis <hey@thomasmcinnis.com>
2026-05-24 21:48:34 +1000

    fix: conjure settings and cleanup

--- config/nvim/lua/plugins/lisp.lua
+++ config/nvim/lua/plugins/lisp.lua
@@ -5,21 +5,16 @@ return {
 		ft = { "racket", "scheme", "sicp", "clojure" },
 		lazy = true,
 		init = function()
-			-- for scheme we use a guile socket, requires starting a guile repl at the relative path below
-			-- then connect with :ConjurConnect
-			vim.g["conjure#filetype#scheme"] = "conjure.client.guile.socket"
-			vim.g["conjure#client#guile#socket#pipename"] = "./.guile-repl.socket"
-			vim.g['conjure#client#guile#socket#enable_completions'] = true
-
 			vim.g["conjure#mapping#doc_word"] = false
-			vim.g["conjure#log#hud#border"] = "rounded"
+			vim.g["conjure#log#hud#border"] = "single"
 			vim.g["conjure#log#hud#anchor"] = "SE"
-			vim.g["conjure#log#hud#width"] = 0.4
-			vim.g["conjure#log#hud#height"] = 0.3
+			vim.g["conjure#log#hud#width"] = 0.3 
+			vim.g["conjure#eval#inline_results"] = true 
 			vim.g["conjure#log#hud#enabled"] = true
 			vim.g["conjure#highlight#enabled"] = true
-			vim.g["conjure#log#strip-ansi-escape-sequences-line-limit"] = 0
+			vim.g["conjure#log#split#height"] = 0.2
 			vim.g["conjure#log#wrap"] = true
+			vim.g["conjure#log#botright"] = true
 		end,
 	},
 	{
