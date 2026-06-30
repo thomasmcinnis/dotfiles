5bcbad58c00f703a98242cff5ede8a2c31a1bcea
Thomas McInnis <thomas.mcinnis@holocentric.com>
2026-06-30 11:04:47 +1000

    feat(nvim): add key clues

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,6 +5,41 @@ vim.pack.add({
     "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
     "https://github.com/nvim-treesitter/nvim-treesitter",
     "https://github.com/alexghergh/nvim-tmux-navigation",
+    "https://github.com/nvim-mini/mini.clue"
+})
+local miniclue = require("mini.clue")
+miniclue.setup({
+    triggers = {
+        -- Leader triggers
+        { mode = { 'n', 'x' }, keys = '<Leader>' },
+        -- `[` and `]` keys
+        { mode = 'n', keys = '[' },
+        { mode = 'n', keys = ']' },
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+        -- `g` key
+        { mode = { 'n', 'x' }, keys = 'g' },
+        -- Marks
+        { mode = { 'n', 'x' }, keys = "'" },
+        { mode = { 'n', 'x' }, keys = '`' },
+        -- Registers
+        { mode = { 'n', 'x' }, keys = '"' },
+        { mode = { 'i', 'c' }, keys = '<C-r>' },
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+        -- `z` key
+        { mode = { 'n', 'x' }, keys = 'z' },
+    },
+    clues = {
+        -- Enhance this by adding descriptions for <Leader> mapping groups
+        miniclue.gen_clues.square_brackets(),
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+    },
 })
 require("vim._core.ui2").enable({}) -- experimental updated messages etc.
 vim.cmd.colorscheme("theme")
@@ -143,10 +178,6 @@ vim.api.nvim_create_autocmd("LspProgress", {
 -- Make j/k move by screen lines when no count is given
 vim.keymap.set("n", "j", function() return vim.v.count == 0 and "gj" or "j" end, { expr = true, silent = true })
 vim.keymap.set("n", "k", function() return vim.v.count == 0 and "gk" or "k" end, { expr = true, silent = true })
-
--- FILE NAVIGATION
-vim.keymap.set("n", "<leader>bn", ":bn<CR>", { desc = "Buffer next", silent = true })
-vim.keymap.set("n", "<leader>bp", ":bp<CR>", { desc = "Buffer prev", silent = true })
 
 -- UI
 vim.keymap.set("n", "<leader>ud",
